3add456bad3706c489be1937b1669d7d76174f1c
Alyssa Ingersoll <alyssa@cyberwit.ch>
2016-01-14 16:55:49 -0800

    Debug is now env var

--- calcounter/settings.py
+++ calcounter/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', ')=^ki@5^22dqme2*l!=5gvq#zmsulh*s7hk585&^nfx8v$3fe=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['localhost', 'calcounter.herokuapp.com']
 
